1a19c95b0b4841a4148bfbb279ed83e7a8717241
Unwana Essien <unwana.essien@outlook.com>
2019-12-14 23:36:04 +0100

    Separate container css from planet circle

--- src/components/Planetary/styles.js
+++ src/components/Planetary/styles.js
@@ -7,15 +7,17 @@ export const Container = styled.div`
     display: flex;
     justify-content: center;
     bottom: -300px;
-    padding: 20px;
+`
+export const CircleBorder = styled.div`
+    display: flex;
+    padding: 30px;
     border-radius: 50%;
-    border: 1px solid #fff;
+    border: 1px solid rgba(255,255,255,0.5);
     width: 600px;
     height: 600px;
 `
 export const Circle = styled.div`
     background-color: #fff;
     width: 600px;
-    height: 600px;
     border-radius: 50%;
 `
